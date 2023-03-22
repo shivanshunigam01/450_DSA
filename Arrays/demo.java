d431da651b0e85999631a4b950d2d1221dd71178
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-22 13:05:04 +0530

    #

--- Arrays/demo.java
+++ Arrays/demo.java
@@ -13,12 +13,11 @@ int sum=0;
 for(int i=0;i<3;++i){
     for(int j=0;j<i+1;++j){
         arr[i][j]=j+1;
-        for(int i=0;i<3;++i ){
-            
-        }
+        System.out.print(arr[i][j]+" ");
+       
     }
 }
-   
+  
 
       
 }
