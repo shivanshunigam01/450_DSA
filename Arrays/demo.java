b4f450a223c2d09a98500d9160586021d95a00d3
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-22 13:02:54 +0530

    22-03-2023

---
+++ Arrays/demo.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class demo {
+   
+private static final String Arrays = null;
+
+public static void main(String[] input) {
+       int arr[] []=new int[4][];
+       arr[0]=new int [1];
+       arr[1]=new int [2];
+       arr[2]=new int [3];
+int sum=0;
+for(int i=0;i<3;++i){
+    for(int j=0;j<i+1;++j){
+        arr[i][j]=j+1;
+        for(int i=0;i<3;++i ){
+            
+        }
+    }
+}
+   
+
+      
+}
+}
