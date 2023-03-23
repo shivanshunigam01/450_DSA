4d25668c3c50a9664165d5adfb3f02649c40de78
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-24 02:57:17 +0530

    24-03-2023

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,8 @@
 
 public class Arrays {
 
+    public static char[] toString(int[] arra) {
+        return null;
+    }
+
 }
---
+++ CyclicArray.java
@@ -0,0 +1,17 @@
+public class CyclicArray {
+   public static void main(String[] args) {
+    int arra[]={1,2,3,4,5};
+    int x=5;
+    for(int i=arra.length-1;i>0;i--){
+
+        arra[i]=arra[i-1];
+        
+    }
+    arra[0]=x;
+  System.out.println(Arrays.toString(arra));
+    
+}
+
+    
+   }
+  
