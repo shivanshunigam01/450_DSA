7016450c1538e3d5b480f7bdf41a55b3e252965f
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-27 16:48:03 +0530

    #

---
+++ Strings/Palaindrome.java
@@ -0,0 +1,27 @@
+package Strings;
+import java.util.*;
+
+class Palaindrome{
+
+
+    public static boolean IsPalindrome(String str){
+        int n=str.length();
+
+        for(int i=0;i<str.length()/2;i++){
+            if(str.charAt(i)==str.charAt(n-1-i)){
+                return true;
+            }
+
+        }
+       
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        String str="NOON";
+        // IsPalindrome(str);
+        System.out.println(IsPalindrome(str));
+       
+    
+}
+}
