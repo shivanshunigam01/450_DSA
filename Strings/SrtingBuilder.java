c1a31a3cf64b61b9c78e0fc6fc23e6de0542d746
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-28 23:42:53 +0530

    #

---
+++ Strings/SrtingBuilder.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class SrtingBuilder {
+    public static void main(String args[]){  
+        StringBuilder sb=new StringBuilder("Hello ");  
+        sb.append("Java");//now original string is changed  
+        System.out.println(sb);//prints Hello Java  
+        }  
+        }  
+
