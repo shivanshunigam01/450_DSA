cdcb9c47137534208ea806c5c5e43d90b013810f
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-04-04 15:41:38 +0530

    #04-04-2023

--- LinkedList/AddList.java
+++ LinkedList/AddList.java
@@ -50,17 +50,35 @@ public class AddList {
         System.out.println("null");
     }
 
+    public void addmiddle(int data, int idx){
+        Node newNode = new Node(data);
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+
+    }
+
     public static void main(String[] args) {
         AddList ll = new AddList();
-        ll.printList();
+        //ll.printList();
         ll.addFirst(2);
-        ll.printList();
+        //ll.printList();
         ll.addFirst(1);
-        ll.printList();
+        //ll.printList();
         ll.addLast(3);
-        ll.printList();
+        //ll.printList();
         ll.addLast(4);
+        //ll.printList()
+        ll.addmiddle(9, 2);
         ll.printList();
+
+
+
     }
 
 }
