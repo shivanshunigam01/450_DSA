e9c463b0cd84bab042c2625dd45dd46e22957341
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-04-09 13:36:52 +0530

    09-04-2023

--- Arrays/subarraywithsum.java
+++ Arrays/subarraywithsum.java
@@ -3,26 +3,46 @@ import java.util.*;
 public class subarraywithsum {
    public static void subarraysum(int arr[],int n,int sum){
    
+    // int count;
+    // for(int i=0;i<n;i++){
+    //     count=arr[i];
+    //     if(count==sum){
+    //         System.out.println(" found at index"+i);
+    //         return;
+    //     }
+    //     else{
+    //         for(int j=i+1;j<n;j++){
+    //             count+=arr[j];
+    //             if(count==sum){
+    //                 System.out.println("found at index"+i+"&"+j);
+    //                 return;
+    //             }
+    //         }
+    //     }
+        
+    // }
+    // System.out.println("not found!");
+    //     return;
     int count;
     for(int i=0;i<n;i++){
         count=arr[i];
         if(count==sum){
-            System.out.println(" found at index"+i);
+            System.out.println("found at index:"+i);
+
             return;
         }
         else{
             for(int j=i+1;j<n;j++){
                 count+=arr[j];
                 if(count==sum){
-                    System.out.println("found at index"+i+"&"+j);
+                    System.out.println("found at indeX:"+i +"&" + j);
                     return;
                 }
             }
         }
-        
     }
     System.out.println("not found!");
-        return;
+    return;
 
 }
     public static void main(String[] args) {
