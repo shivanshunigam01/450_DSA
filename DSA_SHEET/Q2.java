6f6af6681097846b1e4077530b34e2660ce217c6
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-04-10 16:32:25 +0530

    10-04-2023

---
+++ DSA_SHEET/Q2.java
@@ -0,0 +1,10 @@
+public class Q2 {
+    public static void minMax(int array[],int n){
+        int min=Integer.MIN_VALUE;
+    }
+    public static void main(String[] args) {
+        int array[]={1,2,3,4,5,6};
+        int n=array.length;
+
+    }
+}
