f4796dac5a6bfb869cf818bca6e5c2bffaf2f48d
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-04-10 17:18:15 +0530

    #10-04-2023

---
+++ DSA_SHEET/gfg/findTriplet.java
@@ -0,0 +1,50 @@
+package gfg;
+import java.util.*;
+import java.util.Scanner;
+
+public class findTriplet {
+    public static int Triplet(int array[],int n) {
+        Arrays.sort(array);
+        int  count=0;
+        for(int i=n-1;i>=2;i--){
+            int lo=0;
+            int hi=i-1;
+            while(lo<hi){
+                int sum=array[lo]+array[hi];
+                if(sum==array[i]){
+                    count++;
+                    lo++;
+                    hi++;
+                }
+                else if(sum<array[i]){
+                    lo++;
+                }
+                else{
+                    hi--;
+                }
+            }
+          
+        }
+        return count;
+       
+       
+        
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+        int array[]=new int[size];
+        int n=array.length;
+        System.out.println("enter the array elements:");
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+       
+        int result = Triplet(array, n);
+        System.out.println(result);
+        
+        }
+    }
+
